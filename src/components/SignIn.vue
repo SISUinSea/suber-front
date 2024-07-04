35687bfb25dc48b466ae82fc4ca34830ad9204c1
SISU <atgsisu@gmail.com>
2024-07-04 20:54:47 +0900

    refactor: Vuex store 추가, auth 메소드들 store에서 관리

--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -2,7 +2,7 @@
     <div>
       <div>sign in with google</div>
       <img
-        @click="signInWithGoogle()"
+        @click="signIn()"
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/768px-Google_%22G%22_logo.svg.png"
         alt="google logo"
       />
@@ -14,35 +14,10 @@
   
   export default {
     methods: {
-      signInWithGoogle() {
-        const auth = getAuth(firebaseApp);
-  
-        // GoogleAuthProvider 객체 생성 및 Scope 설정
-        const provider = new GoogleAuthProvider();
-        provider.addScope('https://www.googleapis.com/auth/youtube');
-        provider.addScope('https://www.googleapis.com/auth/youtube.force-ssl');
-  
-        //      const result = await signInWithPopup(auth, provider);
-  
-        // Google 로그인 팝업 열기
-        signInWithPopup(auth, provider)
-          .then((result) => {
-            // 구글 로그인 성공 시 처리
-            const credential = GoogleAuthProvider.credentialFromResult(result);
-            const token = credential.accessToken;
-            const user = result.user;
-            //          const googleAccessToken = await user.getIdToken();
-  
-            console.log('Logged in user:', user);
-            this.$router.push('/ready');
-          })
-          .catch((error) => {
-            // 구글 로그인 실패 시 처리
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            console.error('Google login error:', errorCode, errorMessage);
-          });
+      signIn() {
+        this.$store.dispatch('signInWithGoogleAndUpdateUserState');
       },
+      
     },
   };
   </script>
--- src/main.js
+++ src/main.js
@@ -2,21 +2,92 @@ import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
 import { createWebHistory, createRouter } from 'vue-router';
-import HelloWorld from './components/HelloWorld.vue';
 import SignIn from './components/SignIn.vue';
 import Ready from './components/Ready.vue';
+// import store from './store/index.js';
+import { createStore } from 'vuex';
 import firebaseApp from './scripts/firebaseApp.js';
+import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged } from 'firebase/auth';
+
 
 const routes = [
   { path: '/', component: SignIn },
   { path: '/ready', component: Ready },
 ];
 
+const store = createStore( {
+    state() {
+        return {
+            counter : 10,
+            user : null,
+        }
+    },
+    mutations : {
+        setUser(state, user) {
+            state.user = user;
+        },
+
+    },
+    actions : {
+        initAuthState({ commit }) {
+            const auth = getAuth(firebaseApp);
+            onAuthStateChanged(auth, (user) => {
+              if (user) {
+                commit('setUser', user);
+                router.push('/ready');
+              } else {
+                commit('setUser', null);
+                router.push('/');
+              }
+            });
+        },
+        signInWithGoogleAndUpdateUserState(){
+        const auth = getAuth(firebaseApp);
+  
+        // GoogleAuthProvider 객체 생성 및 Scope 설정
+        const provider = new GoogleAuthProvider();
+        provider.addScope('https://www.googleapis.com/auth/youtube');
+        provider.addScope('https://www.googleapis.com/auth/youtube.force-ssl');
+  
+        //      const result = await signInWithPopup(auth, provider);
+  
+        // Google 로그인 팝업 열기
+        signInWithPopup(auth, provider)
+          .then((result) => {
+            // const credential = GoogleAuthProvider.credentialFromResult(result);
+            // this.$store.commit('updateUserState', result.user);
+            const user = result.user;
+            //          const googleAccessToken = await user.getIdToken();
+  
+            console.log('Logged in user:', user);
+            // this.$router.push('/ready');
+          })
+          .catch((error) => {
+            // 구글 로그인 실패 시 처리
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.error('Google login error:', errorCode, errorMessage);
+          });
+        }
+    },
+    getters : {
+        getUserState() {
+            return user;
+        }, 
+        isUserLoggedIn() {
+            return !!user;
+        }
+
+    }
+});
+
+
+
 const router = createRouter({
   history: createWebHistory(),
   routes,
 });
 console.log(firebaseApp);
-createApp(App).use(router).mount('#app');
+createApp(App).use(router).use(store).mount('#app');
 
-//$ npm install -g firebase-tools
+store.dispatch('initAuthState');
