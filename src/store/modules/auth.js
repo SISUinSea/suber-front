d83ed89bae03be37469513d6ee9fedefe7d1d9d8
SISU <101076058+SISUinSea@users.noreply.github.com>
2024-08-08 14:14:43 +0000

    change(auth.js): Firestore error debugging...
    To test how it works in hosting server

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -1,5 +1,5 @@
 import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged, signOut } from 'firebase/auth';
-import { doc, setDoc, getDoc } from 'firebase/firestore';
+import { doc, setDoc, getDoc, updateDoc } from 'firebase/firestore';
 import { getFirestore } from 'firebase/firestore';
 import { firebaseApp } from '@/scripts/firebaseApp';
 import router from '@/router';
@@ -17,23 +17,33 @@ const mutations = {
 };
 
 const actions = {
-  async initAuthState({ commit, dispatch }) {
+  async initAuthState({ commit, dispatch, state }) {
     const auth = getAuth(firebaseApp);
     onAuthStateChanged(auth, async (user) => {
       if (user) {
+        console.log("user uid is : ", user.uid);
         commit('setUser', user);
+        console.log("saved user uid: ", state.user.uid);
         
         // Firestore에서 user의 currentDiveDocRef 확인
         const userDocRef = doc(db, 'users', user.uid);
-        const userDocSnapshot = await getDoc(userDocRef);
+        const userDocSnapshot = await getDoc(userDocRef, { source: "server" });
 
-        if (userDocSnapshot.exists() && userDocSnapshot.data().currentDiveRef) {
+        const userDocData = userDocSnapshot.data();
+
+
+
+
+        console.log("accessToken" in userDocData);
+
+        if (userDocSnapshot.exists() && "currentDiveRef" in userDocData) {
           router.push('/cart');
         } else {
           router.push('/ready');
         }
 
-        if (userDocSnapshot.exists() && !userDocSnapshot.data().consentGiven) {
+        if (!("consentGiven" in userDocData)) {
+          console.log(userDocData);
           router.push('/consent');
         }
       } else {
@@ -49,24 +59,44 @@ const actions = {
     provider.addScope('https://www.googleapis.com/auth/youtube');
     provider.addScope('https://www.googleapis.com/auth/youtube.force-ssl');
     console.log("log in ....");
+
     try {
       const result = await signInWithPopup(auth, provider);
       const credential = GoogleAuthProvider.credentialFromResult(result);
       const accessToken = credential.accessToken;
       const refreshToken = result.user.stsTokenManager.refreshToken;
       const user = result.user;
-      await setDoc(doc(db, 'users', user.uid), {
-        accessToken,
-        refreshToken,
-        tokenExpiry: Date.now() + 3590 * 1000,
-      }, { merge: true });
-      commit('setUser', user);
 
-      // Firestore에서 user의 currentDiveDocRef 확인
       const userDocRef = doc(db, 'users', user.uid);
-      const userDocSnapshot = await getDoc(userDocRef);
-      
+      const docSnapshot = await getDoc(userDocRef, { source: 'server' });
 
+
+      console.log(docSnapshot.data());
+
+      console.log("is documentSnapshot exist?  ", docSnapshot.exists());
+
+
+
+
+      if (!docSnapshot.exists()) {
+        console.log("setDoc");
+        await setDoc(userDocRef, {
+          accessToken,
+          refreshToken,
+          tokenExpiry: Date.now() + 3600 * 1000,
+        }, { merge: true });
+      } else {
+        console.log("updateDoc");
+        await updateDoc(userDocRef, {
+          accessToken,
+          refreshToken,
+          tokenExpiry: Date.now() + 3600 * 1000,
+        });
+      }
+
+      console.log("초기 문서 정보", docSnapshot.data());
+      
+      commit('setUser', user);
     } catch (error) {
       console.error('Google login error:', error);
     }
